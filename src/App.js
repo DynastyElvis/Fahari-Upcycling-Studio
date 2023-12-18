372d9cef06e976d5fa9f5fe76f61e84b2e98c366
Administrator <administrator@Administrators-iMac-3.local>
2023-12-18 11:49:26 +0300

    ft: app routes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
-import ContactPage from './components/ContactPage';
+import Contacts from './components/Contacts';
+import ShopAll from './components/ShopAll';
+import Stories from './components/Stories';
+import Trending from './components/Trending';
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/contact" component={ContactPage} />
+          <Route path="/About" component={AboutPage} />         
+          <Route path="/Contacts" component={Contacts} />
+          <Route path="/HomePage" component={HomePage} />
+          <Route path="/ShopAll" component={ShopAll} />
+          <Route path="/Stories" component={Stories} />
+          <Route path="/Trending" component={Trending} />
           <Redirect to="/" /> {/* Redirect to home page if no matching route */}
         </Switch>
       </div>
---
+++ src/components/Stories.js
@@ -0,0 +1,13 @@
+// src/components/Stories.js
+import React from 'react';
+
+function Stories() {
+  return (
+    <div>
+      <h1>Stories</h1>
+      {/* Add content for the about page */}
+    </div>
+  );
+}
+
+export default Stories;
