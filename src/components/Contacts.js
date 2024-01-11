2cb3636fc095a3e451e95927872faa9b72b79fc7
Administrator <administrator@Administrators-iMac-3.local>
2024-01-11 10:29:41 +0300

    ft: contacts section

---
+++ src/components/Contacts.js
@@ -0,0 +1,13 @@
+// src/components/Contacts.js
+import React from 'react';
+
+function Contacts() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      {/* Add content for the contact page */}
+    </div>
+  );
+}
+
+export default Contacts;
